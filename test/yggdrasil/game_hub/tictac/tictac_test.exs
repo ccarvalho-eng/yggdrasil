92c406482c4995fe554a744f7a2adcc2ac0cd0a3
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2023-10-09 22:02:39 -0300

    test: additional test coverage

--- test/yggdrasil/game_hub/tictac/tictac_test.exs
+++ test/yggdrasil/game_hub/tictac/tictac_test.exs
@@ -7,8 +7,8 @@ defmodule Yggdrasil.GameHub.TictacTest do
   doctest Tictac
 
   describe "find_winning_combination/2" do
-    test "returns winning combination when player did win" do
-      player_0 = Player.build("Kristoff", "X")
+    test "returns winning combination when player wins horizontally" do
+      player = Player.build("Player1", "X")
 
       game = %Tictac{
         board: [
@@ -22,32 +22,73 @@ defmodule Yggdrasil.GameHub.TictacTest do
           %Square{name: :sq32},
           %Square{name: :sq33}
         ],
-        players: [player_0]
+        players: [player]
       }
 
-      assert [:sq11, :sq12, :sq13] == Tictac.find_winning_combination(player_0, game)
+      assert [:sq11, :sq12, :sq13] == Tictac.find_winning_combination(player, game)
+    end
+
+    test "returns winning combination when player wins vertically" do
+      player = Player.build("Player1", "X")
+
+      game = %Tictac{
+        board: [
+          %Square{letter: "X", name: :sq11},
+          %Square{letter: "X", name: :sq21},
+          %Square{letter: "X", name: :sq31},
+          %Square{name: :sq12},
+          %Square{name: :sq22},
+          %Square{name: :sq32},
+          %Square{name: :sq13},
+          %Square{name: :sq23},
+          %Square{name: :sq33}
+        ],
+        players: [player]
+      }
+
+      assert [:sq11, :sq21, :sq31] == Tictac.find_winning_combination(player, game)
+    end
+
+    test "returns winning combination when player wins diagonally" do
+      player = Player.build("Player1", "X")
+
+      game = %Tictac{
+        board: [
+          %Square{letter: "X", name: :sq11},
+          %Square{letter: "X", name: :sq22},
+          %Square{letter: "X", name: :sq33},
+          %Square{name: :sq12},
+          %Square{name: :sq21},
+          %Square{name: :sq23},
+          %Square{name: :sq13},
+          %Square{name: :sq32},
+          %Square{name: :sq31}
+        ],
+        players: [player]
+      }
+
+      assert [:sq11, :sq22, :sq33] == Tictac.find_winning_combination(player, game)
     end
 
     test "returns :not_found when no winning combination" do
-      player_0 = Player.build("Kristoff", "X")
-      player_1 = Player.build("Larah", "O")
+      player = Player.build("Player1", "X")
 
       game = %Tictac{
         board: [
           %Square{letter: "X", name: :sq11},
           %Square{letter: "O", name: :sq12},
-          %Square{letter: "X", name: :sq13},
-          %Square{letter: "X", name: :sq21},
+          %Square{letter: "O", name: :sq13},
+          %Square{letter: "O", name: :sq21},
           %Square{letter: "X", name: :sq22},
           %Square{letter: "O", name: :sq23},
-          %Square{letter: "O", name: :sq31},
+          %Square{letter: "X", name: :sq31},
           %Square{letter: "X", name: :sq32},
           %Square{letter: "O", name: :sq33}
         ],
-        players: [player_0, player_1]
+        players: [player]
       }
 
-      assert :not_found == Tictac.find_winning_combination(player_0, game)
+      assert :not_found == Tictac.find_winning_combination(player, game)
     end
   end
 end
