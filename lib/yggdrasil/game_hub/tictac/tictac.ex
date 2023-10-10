b175a21129cb19e2dbf90ee307080685f25e9902
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2023-10-09 21:46:38 -0300

    doc: add doc back

--- lib/yggdrasil/game_hub/tictac/tictac.ex
+++ lib/yggdrasil/game_hub/tictac/tictac.ex
@@ -31,6 +31,25 @@ defmodule Yggdrasil.GameHub.Tictac do
     field(:board, [Square.t()])
   end
 
+  @doc """
+  Initializes a new game of Tic-Tac-Toe.
+
+  ## Examples
+
+      iex> alias Yggdrasil.GameHub.Tictac.Square
+      iex> alias Yggdrasil.GameHub.Tictac
+      iex> Tictac.init()
+      %Tictac{
+              board: [
+                %Square{letter: nil, name: :sq11}, %Square{letter: nil, name: :sq12}, %Square{letter: nil, name: :sq13},
+                %Square{letter: nil, name: :sq21}, %Square{letter: nil, name: :sq22}, %Square{letter: nil, name: :sq23},
+                %Square{letter: nil, name: :sq31}, %Square{letter: nil, name: :sq32}, %Square{letter: nil, name: :sq33}
+              ],
+              player_turn: nil,
+              players: []
+            }
+
+  """
   @spec init() :: t()
   def init() do
     struct(__MODULE__, %{players: [], player_turn: nil, board: build_board()})
