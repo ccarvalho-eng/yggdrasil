4cf020378e8523b9356d1826f44a3996e365f247
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2023-10-09 21:43:06 -0300

    chore: improve game state

--- lib/yggdrasil/game_hub/tictac/tictac.ex
+++ lib/yggdrasil/game_hub/tictac/tictac.ex
@@ -7,8 +7,23 @@ defmodule Yggdrasil.GameHub.Tictac do
 
   alias Yggdrasil.GameHub.Tictac.{Player, Square}
 
-  @row_size 1..3
-  @col_size 1..3
+  @row_range 1..3
+  @col_range 1..3
+  @winning_combinations [
+    # Straight across wins
+    [:sq11, :sq12, :sq13],
+    [:sq21, :sq22, :sq23],
+    [:sq31, :sq32, :sq33],
+
+    # Vertical wins
+    [:sq11, :sq21, :sq31],
+    [:sq12, :sq22, :sq32],
+    [:sq13, :sq23, :sq33],
+
+    # Diagonal wins
+    [:sq11, :sq22, :sq33],
+    [:sq13, :sq22, :sq31]
+  ]
 
   typedstruct do
     field(:players, [Player.t()])
@@ -16,31 +31,47 @@ defmodule Yggdrasil.GameHub.Tictac do
     field(:board, [Square.t()])
   end
 
-  @doc """
-  Initializes a new game of Tic-Tac-Toe.
-
-  ## Examples
-
-      iex> alias Yggdrasil.GameHub.Tictac.Square
-      iex> alias Yggdrasil.GameHub.Tictac
-      iex> Tictac.init()
-      %Tictac{
-              board: [
-                %Square{letter: nil, name: :sq11}, %Square{letter: nil, name: :sq12}, %Square{letter: nil, name: :sq13},
-                %Square{letter: nil, name: :sq21}, %Square{letter: nil, name: :sq22}, %Square{letter: nil, name: :sq23},
-                %Square{letter: nil, name: :sq31}, %Square{letter: nil, name: :sq32}, %Square{letter: nil, name: :sq33}
-              ],
-              player_turn: nil,
-              players: []
-            }
-
-  """
   @spec init() :: t()
-  def init do
+  def init() do
     struct(__MODULE__, %{players: [], player_turn: nil, board: build_board()})
   end
 
-  defp build_board do
-    for(row <- @row_size, col <- @col_size, do: Square.build(:"sq#{row}#{col}"))
+  @doc """
+  Finds a winning combination for the specified player in the Tic-Tac-Toe game.
+
+  ## Parameters
+
+  - `player`: The player for whom the winning combination is sought.
+  - `game`: The state of the Tic-Tac-Toe game.
+
+  ## Returns
+
+  - A list of atoms representing the winning combination, or
+  - `:not_found` if no winning combination is found.
+
+  """
+  @spec find_winning_combination(Player.t(), t()) :: list(atom()) | :not_found
+  def find_winning_combination(player, game) do
+    case Enum.find(@winning_combinations, &did_player_win?(game.board, player.letter, &1)) do
+      nil -> :not_found
+      combination -> combination
+    end
+  end
+
+  defp build_board() do
+    for row <- @row_range, col <- @col_range, do: Square.build(:"sq#{row}#{col}")
+  end
+
+  defp did_player_win?(board, letter, combination) do
+    Enum.all?(combination, fn square ->
+      has_matching_square?(board, square, letter)
+    end)
+  end
+
+  defp has_matching_square?(board, square_name, letter) do
+    case Enum.find(board, &(&1.name == square_name)) do
+      %Square{letter: ^letter} -> true
+      _ -> false
+    end
   end
 end
--- test/yggdrasil/game_hub/tictac/tictac_test.exs
+++ test/yggdrasil/game_hub/tictac/tictac_test.exs
@@ -1,4 +1,53 @@
 defmodule Yggdrasil.GameHub.TictacTest do
   use ExUnit.Case
-  doctest Yggdrasil.GameHub.Tictac
+
+  alias Yggdrasil.GameHub.Tictac
+  alias Yggdrasil.GameHub.Tictac.{Player, Square}
+
+  doctest Tictac
+
+  describe "find_winning_combination/2" do
+    test "returns winning combination when player did win" do
+      player_0 = Player.build("Kristoff", "X")
+
+      game = %Tictac{
+        board: [
+          %Square{letter: "X", name: :sq11},
+          %Square{letter: "X", name: :sq12},
+          %Square{letter: "X", name: :sq13},
+          %Square{name: :sq21},
+          %Square{name: :sq22},
+          %Square{name: :sq23},
+          %Square{name: :sq31},
+          %Square{name: :sq32},
+          %Square{name: :sq33}
+        ],
+        players: [player_0]
+      }
+
+      assert [:sq11, :sq12, :sq13] == Tictac.find_winning_combination(player_0, game)
+    end
+
+    test "returns :not_found when no winning combination" do
+      player_0 = Player.build("Kristoff", "X")
+      player_1 = Player.build("Larah", "O")
+
+      game = %Tictac{
+        board: [
+          %Square{letter: "X", name: :sq11},
+          %Square{letter: "O", name: :sq12},
+          %Square{letter: "X", name: :sq13},
+          %Square{letter: "X", name: :sq21},
+          %Square{letter: "X", name: :sq22},
+          %Square{letter: "O", name: :sq23},
+          %Square{letter: "O", name: :sq31},
+          %Square{letter: "X", name: :sq32},
+          %Square{letter: "O", name: :sq33}
+        ],
+        players: [player_0, player_1]
+      }
+
+      assert :not_found == Tictac.find_winning_combination(player_0, game)
+    end
+  end
 end
