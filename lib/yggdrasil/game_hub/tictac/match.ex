36d32a0fccedaf0b85a7ac12691b8cc0d51d0a0d
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2023-10-29 08:45:12 -0300

    chore: rename game -> match

--- lib/yggdrasil/game_hub/tictac/match.ex
+++ lib/yggdrasil/game_hub/tictac/match.ex
@@ -1,6 +1,6 @@
 defmodule Yggdrasil.GameHub.Tictac.Match do
   @moduledoc """
-  Module for managing the state of a Tic-Tac-Toe game.
+  Module for managing the state of a Tic-Tac-Toe match.
   """
 
   use TypedStruct
@@ -40,7 +40,7 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
   end
 
   @doc """
-  Initializes a new game of Tic-Tac-Toe.
+  Initializes a new match of Tic-Tac-Toe.
 
   Requires at least one player.
 
@@ -160,7 +160,7 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
       }
 
   """
-  @spec begin(t()) :: {:error, term()} | {:ok, Match.t()}
+  @spec begin(t()) :: {:error, term()} | {:ok, t()}
   def begin(%__MODULE__{status: :playing}), do: {:error, "The match is already in progress."}
   def begin(%__MODULE__{status: :done}), do: {:error, "The match is already over."}
 
@@ -190,9 +190,9 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
   def is_player_turn?(_, _), do: false
 
   @doc """
-  Returns a list of valid moves representing squares on the game board.
+  Returns a list of valid moves representing squares on the match board.
 
-  This function calculates and returns a list of squares that are valid moves based on the current game state.
+  This function calculates and returns a list of squares that are valid moves based on the current match state.
 
   ## Examples
 
@@ -204,19 +204,19 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
 
   """
   @spec get_open_squares(t()) :: list(atom())
-  def get_open_squares(game) do
-    Enum.reduce(game.board, [], fn square, acc ->
+  def get_open_squares(match) do
+    Enum.reduce(match.board, [], fn square, acc ->
       if Square.is_open?(square), do: [square.name | acc], else: acc
     end)
   end
 
   @doc """
-  Finds a winning combination for the specified player in the Tic-Tac-Toe game.
+  Finds a winning combination for the specified player in the Tic-Tac-Toe match.
 
   ## Parameters
 
   - `player`: The player for whom the winning combination is sought.
-  - `game`: The state of the Tic-Tac-Toe game.
+  - `match`: The state of the Tic-Tac-Toe match.
 
   ## Returns
 
@@ -225,26 +225,26 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
 
   """
   @spec find_winning_combination(Player.t(), t()) :: list(atom()) | :not_found
-  def find_winning_combination(player, game) do
-    case Enum.find(@winning_combinations, &did_player_win?(game, player, &1)) do
+  def find_winning_combination(player, match) do
+    case Enum.find(@winning_combinations, &did_player_win?(match, player, &1)) do
       nil -> :not_found
       combination -> combination
     end
   end
 
   @doc """
-  Determines if the game is over based on the current game state.
+  Determines if the match is over based on the current match state.
 
-  This function checks if the game has reached its end by evaluating two conditions:
+  This function checks if the match has reached its end by evaluating two conditions:
 
-  1. Whether any player has won the game.
-  2. Whether there are no open squares left on the game board.
+  1. Whether any player has won the match.
+  2. Whether there are no open squares left on the match board.
 
   ## Examples
 
       iex> alias Yggdrasil.GameHub.Tictac.Square
       iex> alias Yggdrasil.GameHub.Tictac.Match
-      iex> game = %Match{
+      iex> match = %Match{
       ...>   board: [
       ...>     %Square{letter: "X", name: :sq11},
       ...>     %Square{letter: "X", name: :sq12},
@@ -262,14 +262,14 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
       ...>     %Player{letter: "O", name: "Kristoff"}
       ...>   ]
       ...> }
-      iex> Match.game_over?(game)
+      iex> Match.match_over?(match)
       true
 
   """
-  @spec game_over?(t()) :: boolean()
-  def game_over?(game) do
-    a_player_won = did_any_player_win?(game)
-    no_open_squares = game |> get_open_squares() |> Enum.empty?()
+  @spec match_over?(t()) :: boolean()
+  def match_over?(match) do
+    a_player_won = did_any_player_win?(match)
+    no_open_squares = match |> get_open_squares() |> Enum.empty?()
 
     a_player_won || no_open_squares
   end
@@ -278,15 +278,15 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
     for row <- @row_range, col <- @col_range, do: Square.build(:"sq#{row}#{col}")
   end
 
-  defp did_player_win?(game, player, combination) do
+  defp did_player_win?(match, player, combination) do
     Enum.all?(combination, fn square ->
-      has_matching_square?(game.board, square, player.letter)
+      has_matching_square?(match.board, square, player.letter)
     end)
   end
 
-  defp did_any_player_win?(game) do
-    Enum.any?(game.players, fn player ->
-      case find_winning_combination(player, game) do
+  defp did_any_player_win?(match) do
+    Enum.any?(match.players, fn player ->
+      case find_winning_combination(player, match) do
         :not_found -> false
         [_, _, _] -> true
       end
