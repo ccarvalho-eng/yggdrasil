459b191e7c88324d0c472e47d0d1655dafd088f1
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2023-11-21 14:03:44 -0300

    Extra Helpers for Match context (#12)

--- lib/yggdrasil/game_hub/tictac/match.ex
+++ lib/yggdrasil/game_hub/tictac/match.ex
@@ -1,6 +1,6 @@
 defmodule Yggdrasil.GameHub.Tictac.Match do
   @moduledoc """
-  Module for managing the state of a Tic-Tac-Toe game.
+  Module for managing the state of a Tic-Tac-Toe match.
   """
 
   use TypedStruct
@@ -25,20 +25,30 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
     [:sq13, :sq22, :sq31]
   ]
 
+  defmodule Status do
+    @moduledoc """
+    Match status.
+    """
+    @type t :: :not_started | :playing | :done
+  end
+
   typedstruct do
     field(:players, [Player.t()])
-    field(:player_turn, integer() | nil)
+    field(:player_turn, binary())
     field(:board, [Square.t()])
+    field(:status, Status.t(), default: :not_started)
   end
 
   @doc """
-  Initializes a new game of Tic-Tac-Toe.
+  Initializes a new match of Tic-Tac-Toe.
+
+  Requires at least one player.
 
   ## Examples
 
-      iex> alias Yggdrasil.GameHub.Tictac.Square
-      iex> alias Yggdrasil.GameHub.Tictac.Match
-      iex> Match.init()
+      iex> alias Yggdrasil.GameHub.Tictac.{Match, Player, Square}
+      iex> player = Player.build("Kristoff", "X")
+      iex> Match.init(player)
       %Match{
         board: [
           %Square{letter: nil, name: :sq11},
@@ -52,42 +62,161 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
           %Square{letter: nil, name: :sq33}
         ],
         player_turn: nil,
-        players: []
+        players: [player],
+        status: :not_started
       }
 
   """
-  @spec init() :: t()
-  def init do
-    struct(__MODULE__, %{players: [], player_turn: nil, board: build_board()})
+  @spec init(Player.t()) :: t()
+  def init(player) do
+    struct(__MODULE__, %{players: [player], player_turn: nil, board: build_board()})
   end
 
   @doc """
-  Returns a list of valid moves representing squares on the game board.
+  Adds a player to a match.
 
-  This function calculates and returns a list of squares that are valid moves based on the current game state.
+  - Can't add a player if the match wasn't initialized by another player.
+  - Only two players are allowed in the match.
 
   ## Examples
 
-      iex> alias Yggdrasil.GameHub.Tictac.Match
-      iex> game = Match.init()
-      iex> Match.get_open_squares(game)
+      iex> alias Yggdrasil.GameHub.Tictac.{Match, Player, Square}
+      iex> player_1 = Player.build("Kristoff", "X")
+      iex> player_2 = Player.build("Larah", "X")
+      iex> match = Match.init(player_1)
+      iex> Match.join(match, player_2)
+      {:ok,
+        %Match{
+          board: [
+            %Square{letter: nil, name: :sq11},
+            %Square{letter: nil, name: :sq12},
+            %Square{letter: nil, name: :sq13},
+            %Square{letter: nil, name: :sq21},
+            %Square{letter: nil, name: :sq22},
+            %Square{letter: nil, name: :sq23},
+            %Square{letter: nil, name: :sq31},
+            %Square{letter: nil, name: :sq32},
+            %Square{letter: nil, name: :sq33}
+          ],
+          player_turn: nil,
+          players: [
+            %Player{letter: "X", name: "Kristoff"},
+            %Player{letter: "O", name: "Larah"}
+          ],
+          status: :not_started
+        }
+      }
+      iex> match = %Match{players: []}
+      iex> Match.join(match, player_2)
+      {:error, "You can only join a pre-existing match."}
+      iex> match = %Match{players: [player_1, player_2]}
+      iex> player_3 = Player.build("Ruth", "X")
+      iex> Match.join(match, player_3)
+      {:error, "Only two players are permitted."}
+
+  """
+  @spec join(t(), Player.t()) :: {:error, String.t()}
+  def join(%__MODULE__{players: []}, _player),
+    do: {:error, "You can only join a pre-existing match."}
+
+  def join(%__MODULE__{players: [_player_1, _player_2]}, _player_3),
+    do: {:error, "Only two players are permitted."}
+
+  def join(%__MODULE__{players: [%{letter: l} = player_1]} = match, %{letter: l} = player_2) do
+    player_2 = %Player{player_2 | letter: swap_letter(player_2)}
+    {:ok, %__MODULE__{match | players: [player_1, player_2]}}
+  end
+
+  def join(%__MODULE__{players: [player_1]} = match, player_2) do
+    {:ok, %__MODULE__{match | players: [player_1, player_2]}}
+  end
+
+  @doc """
+  Begins a match if it's not already started or completed.
+
+  ## Examples
+
+      iex> alias Yggdrasil.GameHub.Tictac.{Match, Player}
+      iex> match = %Match{status: :playing}
+      iex> Match.begin(match)
+      {:error, "The match is already in progress."}
+      iex> match = %Match{status: :done}
+      iex> Match.begin(match)
+      {:error, "The match is already over."}
+      iex> player_1 = Player.build("Kristoff", "X")
+      iex> player_2 = Player.build("Larah", "O")
+      iex> match = %Match{status: :not_started, players: [player_1, player_2]}
+      iex> Match.begin(match)
+      {:ok,
+        %Match{
+          status: :playing,
+          board: nil,
+          player_turn: "O",
+          players: [
+            %Player{letter: "X", name: "Kristoff"},
+            %Player{letter: "O", name: "Larah"}
+          ]
+        }
+      }
+
+  """
+  @spec begin(t()) :: {:error, String.t()} | {:ok, t()}
+  def begin(%__MODULE__{status: :playing}), do: {:error, "The match is already in progress."}
+  def begin(%__MODULE__{status: :done}), do: {:error, "The match is already over."}
+
+  def begin(%__MODULE__{status: :not_started, players: [_player_1, _player_2]} = match) do
+    {:ok, %__MODULE__{match | status: :playing, player_turn: "O"}}
+  end
+
+  def begin(_), do: {:error, "Not enough players for the match."}
+
+  @doc """
+  Returns true if it's the player's turn.
+
+  ## Examples
+
+      iex> alias Yggdrasil.GameHub.Tictac.{Match, Player}
+      iex> match = %Match{player_turn: "X"}
+      iex> player_1 = Player.build("Kristoff", "X")
+      iex> player_2 = Player.build("Larah", "O")
+      iex> Match.is_player_turn?(match, player_1)
+      true
+      iex> Match.is_player_turn?(match, player_2)
+      false
+
+  """
+  @spec is_player_turn?(t(), Player.t()) :: boolean()
+  def is_player_turn?(%__MODULE__{player_turn: letter}, %Player{letter: letter}), do: true
+  def is_player_turn?(_, _), do: false
+
+  @doc """
+  Returns a list of valid moves representing squares on the match board.
+
+  This function calculates and returns a list of squares that are valid moves based on the current match state.
+
+  ## Examples
+
+      iex> alias Yggdrasil.GameHub.Tictac.{Match, Player}
+      iex> player = Player.build("Kristoff", "X")
+      iex> match = Match.init(player)
+      iex> Match.get_open_squares(match)
       [:sq33, :sq32, :sq31, :sq23, :sq22, :sq21, :sq13, :sq12, :sq11]
 
   """
   @spec get_open_squares(t()) :: list(atom())
-  def get_open_squares(game) do
-    Enum.reduce(game.board, [], fn square, acc ->
+  def get_open_squares(match) do
+    Enum.reduce(match.board, [], fn square, acc ->
       if Square.is_open?(square), do: [square.name | acc], else: acc
     end)
   end
 
   @doc """
-  Finds a winning combination for the specified player in the Tic-Tac-Toe game.
+  Finds a winning combination for the specified player in the Tic-Tac-Toe match.
 
   ## Parameters
 
+  - `match`: The state of the Tic-Tac-Toe match.
   - `player`: The player for whom the winning combination is sought.
-  - `game`: The state of the Tic-Tac-Toe game.
 
   ## Returns
 
@@ -95,27 +224,27 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
   - `:not_found` if no winning combination is found.
 
   """
-  @spec find_winning_combination(Player.t(), t()) :: list(atom()) | :not_found
-  def find_winning_combination(player, game) do
-    case Enum.find(@winning_combinations, &did_player_win?(game, player, &1)) do
+  @spec find_winning_combination(t(), Player.t()) :: list(atom()) | :not_found
+  def find_winning_combination(match, player) do
+    case Enum.find(@winning_combinations, &did_player_win?(match, player, &1)) do
       nil -> :not_found
       combination -> combination
     end
   end
 
   @doc """
-  Determines if the game is over based on the current game state.
+  Determines if the match is over based on the current match state.
 
-  This function checks if the game has reached its end by evaluating two conditions:
+  This function checks if the match has reached its end by evaluating two conditions:
 
-  1. Whether any player has won the game.
-  2. Whether there are no open squares left on the game board.
+  1. Whether any player has won the match.
+  2. Whether there are no open squares left on the match board.
 
   ## Examples
 
       iex> alias Yggdrasil.GameHub.Tictac.Square
       iex> alias Yggdrasil.GameHub.Tictac.Match
-      iex> game = %Match{
+      iex> match = %Match{
       ...>   board: [
       ...>     %Square{letter: "X", name: :sq11},
       ...>     %Square{letter: "X", name: :sq12},
@@ -133,31 +262,58 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
       ...>     %Player{letter: "O", name: "Kristoff"}
       ...>   ]
       ...> }
-      iex> Match.game_over?(game)
+      iex> Match.is_match_over?(match)
       true
 
   """
-  @spec game_over?(t()) :: boolean()
-  def game_over?(game) do
-    a_player_won = did_any_player_win?(game)
-    no_open_squares = game |> get_open_squares() |> Enum.empty?()
+  @spec is_match_over?(t()) :: boolean()
+  def is_match_over?(match), do: did_any_player_win?(match) || draw?(match)
 
-    a_player_won || no_open_squares
+  @doc """
+  Returns the match's result.
+  """
+  @spec result(t()) :: :draw | :playing | Player.t()
+  def result(%__MODULE__{players: [player_1, player_2]} = match) do
+    cond do
+      did_player_win?(match, player_1) -> player_1
+      did_player_win?(match, player_2) -> player_2
+      draw?(match) -> :draw
+      true -> :playing
+    end
+  end
+
+  @spec play(t(), Player.t(), Square.t()) :: {:ok, t()} | {:error, String.t()}
+  def play({:ok, %__MODULE__{} = match}, player, square), do: play(match, player, square)
+
+  def play(match, player, square) do
+    match
+    |> validate_player_turn(player)
+    |> validate_square(square)
+    |> put_letter(player, square)
+    |> check_match_status()
+    |> next_player_turn()
   end
 
   defp build_board do
     for row <- @row_range, col <- @col_range, do: Square.build(:"sq#{row}#{col}")
   end
 
-  defp did_player_win?(game, player, combination) do
+  defp did_player_win?(match, player) do
+    case find_winning_combination(match, player) do
+      [_, _, _] -> true
+      _ -> false
+    end
+  end
+
+  defp did_player_win?(match, player, combination) do
     Enum.all?(combination, fn square ->
-      has_matching_square?(game.board, square, player.letter)
+      has_matching_square?(match.board, square, player.letter)
     end)
   end
 
-  defp did_any_player_win?(game) do
-    Enum.any?(game.players, fn player ->
-      case find_winning_combination(player, game) do
+  defp did_any_player_win?(match) do
+    Enum.any?(match.players, fn player ->
+      case find_winning_combination(match, player) do
         :not_found -> false
         [_, _, _] -> true
       end
@@ -170,4 +326,55 @@ defmodule Yggdrasil.GameHub.Tictac.Match do
       _ -> false
     end
   end
+
+  defp draw?(match), do: match |> get_open_squares() |> Enum.empty?()
+
+  defp swap_letter(%Player{letter: "O"}), do: "X"
+  defp swap_letter(_), do: "O"
+
+  defp validate_player_turn(match, player) do
+    if is_player_turn?(match, player), do: {:ok, match}, else: {:error, "Not player's turn."}
+  end
+
+  defp validate_square({:ok, %__MODULE__{} = match}, square) do
+    if Square.is_open?(square), do: {:ok, match}, else: {:error, "Square is not open."}
+  end
+
+  defp validate_square(error, _), do: error
+
+  defp put_letter({:ok, %__MODULE__{} = match}, player, square) do
+    updated_board = Enum.map(match.board, &update_square(&1, square.name, player))
+
+    {:ok, %__MODULE__{match | board: updated_board}}
+  end
+
+  defp put_letter(error, _, _), do: error
+
+  defp update_square(%Square{name: square_name} = sq, square_name, player),
+    do: %Square{sq | letter: player.letter}
+
+  defp update_square(sq, _, _),
+    do: sq
+
+  defp check_match_status({:ok, %__MODULE__{} = match}) do
+    case result(match) do
+      :playing ->
+        {:ok, match}
+
+      _ ->
+        {:ok, %__MODULE__{match | status: :done}}
+    end
+  end
+
+  defp check_match_status(error), do: error
+
+  defp next_player_turn({:ok, %__MODULE__{player_turn: "X"} = match}) do
+    {:ok, %__MODULE__{match | player_turn: "O"}}
+  end
+
+  defp next_player_turn({:ok, %__MODULE__{player_turn: "O"} = match}) do
+    {:ok, %__MODULE__{match | player_turn: "X"}}
+  end
+
+  defp next_player_turn(error), do: error
 end
--- test/yggdrasil/game_hub/tictac/match_test.exs
+++ test/yggdrasil/game_hub/tictac/match_test.exs
@@ -8,46 +8,129 @@ defmodule Yggdrasil.GameHub.Tictac.MatchTest do
   @horizontal_wins [[:sq11, :sq12, :sq13], [:sq21, :sq22, :sq23], [:sq31, :sq32, :sq33]]
   @vertical_wins [[:sq11, :sq21, :sq31], [:sq12, :sq22, :sq32], [:sq13, :sq23, :sq33]]
   @diagonal_wins [[:sq11, :sq22, :sq33], [:sq13, :sq22, :sq31]]
+  @random_win Enum.random(@horizontal_wins ++ @vertical_wins ++ @diagonal_wins)
 
   setup do
-    player = Player.build("Player1", "X")
-    game = %Match{players: [player]}
-    {:ok, player: player, game: game}
+    player_1 = Player.build("Kristoff", "X")
+    player_2 = Player.build("Larah", "O")
+
+    {:ok, match} = player_1 |> Match.init() |> Match.join(player_2)
+    {:ok, match: match, player_1: player_1, player_2: player_2}
   end
 
-  for horizontal_win <- @horizontal_wins do
-    description = "returns #{Enum.join(horizontal_win, ", ")} when player wins horizontally"
+  describe "find_winning_combinations/2" do
+    setup %{match: match, player_1: player} do
+      {:ok, player: player, match: match}
+    end
 
-    test description, %{player: player, game: game} do
-      assert_winning_combination(player, game, unquote(horizontal_win))
+    for horizontal_win <- @horizontal_wins do
+      description = "returns #{Enum.join(horizontal_win, ", ")} when player wins horizontally"
+
+      test description, %{player: player, match: match} do
+        assert_winning_combination(match, player, unquote(horizontal_win))
+      end
+    end
+
+    for vertical_win <- @vertical_wins do
+      description = "returns #{Enum.join(vertical_win, ", ")} when player wins vertically"
+
+      test description, %{player: player, match: match} do
+        assert_winning_combination(match, player, unquote(vertical_win))
+      end
+    end
+
+    for diagonal_win <- @diagonal_wins do
+      description = "returns #{Enum.join(diagonal_win, ", ")} when player wins diagonally"
+
+      test description, %{player: player, match: match} do
+        assert_winning_combination(match, player, unquote(diagonal_win))
+      end
+    end
+
+    test "returns :not_found when no winning combination", %{player: player, match: match} do
+      updated_match = Map.update!(match, :board, fn _ -> [] end)
+      assert Match.find_winning_combination(updated_match, player) == :not_found
     end
   end
 
-  for vertical_win <- @vertical_wins do
-    description = "returns #{Enum.join(vertical_win, ", ")} when player wins vertically"
+  describe "result/2" do
+    test "returns match winner", %{match: match, player_1: player_1, player_2: player_2} do
+      for player <- [player_1, player_2] do
+        winning_combination = Enum.map(@random_win, &%Square{letter: player.letter, name: &1})
+        match = %Match{match | board: winning_combination}
 
-    test description, %{player: player, game: game} do
-      assert_winning_combination(player, game, unquote(vertical_win))
+        assert Match.result(match) == player
+      end
+    end
+
+    test "returns `:draw` when no winning combinations", %{match: match} do
+      draw_combination = [
+        %Square{letter: "X", name: :sq11},
+        %Square{letter: "O", name: :sq12},
+        %Square{letter: "X", name: :sq13},
+        %Square{letter: "X", name: :sq21},
+        %Square{letter: "O", name: :sq22},
+        %Square{letter: "O", name: :sq23},
+        %Square{letter: "O", name: :sq31},
+        %Square{letter: "X", name: :sq32},
+        %Square{letter: "O", name: :sq33}
+      ]
+
+      match = %Match{match | board: draw_combination}
+      assert Match.result(match) == :draw
+    end
+
+    test "returns `:playing` if match is not over", %{match: match} do
+      assert Match.result(match) == :playing
     end
   end
 
-  for diagonal_win <- @diagonal_wins do
-    description = "returns #{Enum.join(diagonal_win, ", ")} when player wins diagonally"
+  describe "play/3" do
+    test "returns error when it's not the player's turn", %{
+      player_1: player_1,
+      player_2: player_2
+    } do
+      match = %Match{player_turn: player_2.letter}
+      square = %Square{}
 
-    test description, %{player: player, game: game} do
-      assert_winning_combination(player, game, unquote(diagonal_win))
+      assert Match.play(match, player_1, square) == {:error, "Not player's turn."}
+    end
+
+    test "returns error when square is not open", %{player_1: player} do
+      match = %Match{player_turn: player.letter}
+      square = %Square{letter: "X", name: :sq11}
+
+      assert Match.play(match, player, square) == {:error, "Square is not open."}
+    end
+
+    test "updates board with player's letter", %{player_1: player_1, player_2: player_2} do
+      match = %Match{
+        player_turn: player_1.letter,
+        board: [%Square{letter: nil, name: :sq11}],
+        players: [player_1, player_2]
+      }
+
+      player_2_letter = player_2.letter
+      square = %Square{letter: nil, name: :sq11}
+
+      assert {
+               :ok,
+               %Match{
+                 board: [%Square{letter: "X", name: :sq11}],
+                 player_turn: ^player_2_letter,
+                 players: [
+                   %Player{letter: "X", name: "Kristoff"},
+                   %Player{letter: "O", name: "Larah"}
+                 ]
+               }
+             } = Match.play(match, player_1, square)
     end
   end
 
-  test "returns :not_found when no winning combination", %{player: player, game: game} do
-    updated_game = Map.update!(game, :board, fn _ -> [] end)
-    assert Match.find_winning_combination(player, updated_game) == :not_found
-  end
-
-  defp assert_winning_combination(player, game, win_combination) do
+  defp assert_winning_combination(match, player, win_combination) do
     squares = Enum.map(win_combination, &%Square{letter: "X", name: &1})
-    updated_game = Map.update!(game, :board, fn _ -> squares end)
+    updated_match = Map.update!(match, :board, fn _ -> squares end)
 
-    assert Match.find_winning_combination(player, updated_game) == win_combination
+    assert Match.find_winning_combination(updated_match, player) == win_combination
   end
 end
